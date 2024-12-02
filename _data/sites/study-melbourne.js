1d010b1126732b0287a57a98aa1b2307f611c792
Ian James <ian.james@ecodev.vic.gov.au>
2024-12-02 11:49:19 +1100

    feat: add top study melbourne pages

---
+++ _data/sites/study-melbourne.js
@@ -0,0 +1,23 @@
+module.exports = {
+	name: 'Study Melbourne',
+	description: 'Top ten pages for Study Melbourne',
+	skip: false,
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		'https://studymelbourne.vic.gov.au/au',
+		'https://studymelbourne.vic.gov.au/ke',
+		'https://studymelbourne.vic.gov.au/?toggle=intl',
+		'https://studymelbourne.vic.gov.au/about-study-melbourne',
+		'https://studymelbourne.vic.gov.au/why-melbourne',
+		'https://studymelbourne.vic.gov.au/our-study-hubs/study-melbourne-hub-victoria',
+		'https://studymelbourne.vic.gov.au/news',
+		'https://studymelbourne.vic.gov.au/events',
+		'https://studymelbourne.vic.gov.au/about-study-melbourne/victorian-international-education-awards',
+		'https://studymelbourne.vic.gov.au/about-study-melbourne/victorian-international-education-awards/2024-student-winners-and-finalists',
+		'https://studymelbourne.vic.gov.au/study-with-us/find-a-course',
+		'https://studymelbourne.vic.gov.au/study-with-us/types-of-education/our-universities',
+	]
+}
